460cdba1a8e5936c9b7ec4e674b9472b55a3a888
Rapidcognition <37350380+Rapidcognition@users.noreply.github.com>
2018-10-18 15:34:33 +0200

    Stuff

--- Butikv3.5/MyForm.cs
+++ Butikv3.5/MyForm.cs
@@ -5,18 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
+using System.IO;
 
 namespace Butikv3._5
 {
+    class Product
+    {
+        public int ID;
+        public string name;
+        public string item;
+        public string description;
+        public PictureBox pictureBox;
+    }
     class MyForm : Form
     {
         #region Main form variables
         TableLayoutPanel mainPanel;
         TableLayoutPanel topLeftSidePanel;
         TableLayoutPanel topRightSidePanel;
+        TableLayoutPanel bottomLeftSidePanel;
+        FlowLayoutPanel bottomLeftSideInnerPanel;
         TextBox searchBox;
+        Button itemButton;
+        Button homeButton;
         #endregion
 
+        List<Product> list = new List<Product>();
+        List<string> listItem = new List<string>();
         public MyForm()
         {
             this.MinimumSize = new Size(800, 500);
@@ -25,6 +40,7 @@ namespace Butikv3._5
 
             mainPanel = new TableLayoutPanel
             {
+                Margin = new Padding(0, 0, 0, 0),
                 Dock = DockStyle.Fill,
                 BackColor = Color.Blue,
                 CellBorderStyle = TableLayoutPanelCellBorderStyle.Single,
@@ -36,8 +52,11 @@ namespace Butikv3._5
             mainPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 100));
             mainPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 70));
 
+            Controls.Add(mainPanel);
+
             topLeftSidePanel = new TableLayoutPanel
             {
+                Margin = new Padding(0, 0, 0, 0),
                 Dock = DockStyle.Fill,
                 BackColor = Color.Bisque,
                 RowCount = 2,
@@ -45,8 +64,19 @@ namespace Butikv3._5
             topLeftSidePanel.RowStyles.Add(new RowStyle(SizeType.Percent, 60));
             topLeftSidePanel.RowStyles.Add(new RowStyle(SizeType.Percent, 40));
 
+            mainPanel.Controls.Add(topLeftSidePanel);
+
+            Button searchLabel = new Button { Text = "Filter items.", Dock = DockStyle.Fill, TextAlign = ContentAlignment.MiddleCenter, };
+            searchBox = new TextBox
+            {
+                Dock = DockStyle.Fill,
+            };
+            topLeftSidePanel.Controls.Add(searchLabel);
+            topLeftSidePanel.Controls.Add(searchBox);
+
             topRightSidePanel = new TableLayoutPanel
             {
+                Margin = new Padding(0, 0, 0, 0),
                 ColumnCount = 3,
                 Dock = DockStyle.Fill,
                 BackColor = Color.BlanchedAlmond,
@@ -54,20 +84,83 @@ namespace Butikv3._5
             topRightSidePanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 60));
             topRightSidePanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20));
             topRightSidePanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20));
-
-            Controls.Add(mainPanel);
-            mainPanel.Controls.Add(topLeftSidePanel);
             mainPanel.Controls.Add(topRightSidePanel);
-            Label searchLabel = new Label { Text = "Filter items.", Dock = DockStyle.Fill, TextAlign = ContentAlignment.MiddleCenter, };
-            searchBox = new TextBox
+            
+            bottomLeftSidePanel = new TableLayoutPanel
             {
                 Dock = DockStyle.Fill,
+                Margin = new Padding(0,0,0,0),
+                BackColor = Color.Bisque,
+                RowCount = 2,
             };
-            topLeftSidePanel.Controls.Add(searchLabel);
-            topLeftSidePanel.Controls.Add(searchBox);
+            bottomLeftSidePanel.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 150));
+            bottomLeftSidePanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 80));
+            bottomLeftSidePanel.RowStyles.Add(new RowStyle(SizeType.AutoSize, 100));
+
+            mainPanel.Controls.Add(bottomLeftSidePanel);
+
+            homeButton = new Button { Text = "Home", Dock = DockStyle.Fill, BackColor = Color.Crimson, };
+            bottomLeftSidePanel.Controls.Add(homeButton);
+            bottomLeftSidePanel.Controls.Add(bottomLeftSideInnerPanel);
+
+            bottomLeftSideInnerPanel = new FlowLayoutPanel
+            {
+                Margin = new Padding(0, 0, 0, 0),
+                BackColor = Color.Orange,
+                Dock = DockStyle.Fill
+            };
+
+
             #endregion
+        }
+        private void QueryFromCSVToList()
+        {
+            string[][] path = File.ReadAllLines(@"TextFile1.csv").Select(x => x.Split(',')).Where(x => x[0] != "" && x[1] != "" && x[2] != "" && x[3] != "").ToArray();
+
+            foreach (var item in path) //Läs in olika genre i lista
+            {
+                if (!listItem.Contains(item[2]))
+                    listItem.Add(item[2]);
+            }
+            foreach (var item in listItem)
+            {
+                itemButton = new Button
+                {
+                    Text = item
+                };
+                itemButton.Click += ItemButton_Click;
+                bottomLeftSideInnerPanel.Controls.Add(itemButton);
+                // Implementera vart inlästa knappar ska hamna i controls och gör clickevent.
+
+            } // Slut på dynamiska genre-inläsningen
+            for (int i = 0; i < path.Length; i++)
+            {
+                Product tmp = new Product
+                {
+                    ID = int.Parse(path[i][0]),
+                    name = path[i][1],
+                    item = path[i][2],
+                    description = path[i][3],
+
+                    // Fix pictureBox imageLocation.
+                    pictureBox = new PictureBox
+                    {
+                        Dock = DockStyle.Fill,
+                        Height = 150,
+                        Width = 150,
+                        SizeMode = PictureBoxSizeMode.StretchImage,
+                        ImageLocation = @"Movies\" + i + ".jpg"
+                    }
+                };
+                list.Add(tmp);
+            }
+        }
 
 
+        // Funktion för att filtera produkter baserat på vilken knapp som klickas ned.
+        private void ItemButton_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
