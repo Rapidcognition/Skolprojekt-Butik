14717d759f00db254c8a1ca07d5849e422f61e02
Samuel Drysén <samuel.drysen@live.se>
2018-10-19 11:32:11 +0200

    Minor cart changes

--- Butikv3.5/MainView/CartPanel.cs
+++ Butikv3.5/MainView/CartPanel.cs
@@ -10,19 +10,79 @@ namespace Butikv3._5
 {
     class CartPanel : TableLayoutPanel
     {
+        private class CartItem : TableLayoutPanel
+        {
+            private Product product;
+
+            public CartItem(Product toCartItem)
+            {
+                this.product = toCartItem;
+
+                this.Dock = DockStyle.Top;
+                this.ColumnCount = 3;
+                this.Size = new Size(200, 50);
+                this.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+                this.Margin = new Padding(0);
+                this.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25));
+                this.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50));
+                this.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25));
+                
+
+                PictureBox picture = new PictureBox
+                {
+                    ImageLocation = toCartItem.pictureBox.ImageLocation,
+                    Dock = DockStyle.Left,
+                    Size = new Size(32, 32),
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                };
+                this.Controls.Add(picture);
+
+                Label itemLabel = new Label
+                {
+                    Text = toCartItem.name,
+                    AutoSize = true,
+                    Anchor = AnchorStyles.Top,
+                };
+                this.Controls.Add(itemLabel);
+
+                NumericUpDown itemCount = new NumericUpDown
+                {
+                    Dock = DockStyle.Right,
+                };
+                this.Controls.Add(itemCount);
+            }
+
+            public Product GetProduct()
+            {
+                return product;
+            }
+        };
+
+        private TableLayoutPanel leftPanel;
+        private TableLayoutPanel middlePanel;
+        private TableLayoutPanel rightPanel;
+        private List<CartItem> cartItems = new List<CartItem>();
+
         public CartPanel()
         {
             this.Name = "Cart";
             this.Dock = DockStyle.Fill;
             this.ColumnCount = 3;
-            this.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25));
-            this.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50));
-            this.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25));
+            this.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 200));
+            this.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 40));
+            this.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 200));
             this.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
             this.Margin = new Padding(0);
             //---
             this.BackColor = Color.White;
             //---
+            leftPanel = new TableLayoutPanel
+            {
+                Dock = DockStyle.Fill,
+                Margin = new Padding(0),
+                AutoScroll = true,
+            };
+            this.Controls.Add(leftPanel);
 
             Button tempAddButton = new Button
             {
@@ -42,38 +102,29 @@ namespace Butikv3._5
 
         public void AddToCart(Product toCart)
         {
-            TableLayoutPanel panel = new TableLayoutPanel
-            {
-                Dock = DockStyle.Top,
-                Size = new Size(ClientSize.Width, 50),
-                BorderStyle = BorderStyle.FixedSingle,
-                Margin = new Padding(0),
-                ColumnCount = 3,
-                CellBorderStyle = TableLayoutPanelCellBorderStyle.Single,
-            };
-            panel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25));
-            panel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50));
-            panel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25));
+            CartItem temp = new CartItem(toCart);
 
-            PictureBox picture = new PictureBox
-            {
-                ImageLocation = toCart.pictureBox.ImageLocation,
-                Dock = DockStyle.Left,
-                Size = new Size(32,32),
-                SizeMode = PictureBoxSizeMode.Zoom,
-                
-            };
-            Label itemLabel = new Label
-            {
-                Text = toCart.name,
-                AutoSize = true,
-                Anchor = AnchorStyles.Top,
-                
-            };
 
-            panel.Controls.Add(picture);
-            panel.Controls.Add(itemLabel);
-            this.Controls.Add(panel);
+            if(cartItems.Count == 0)
+            {
+                cartItems.Add(temp);
+                leftPanel.Controls.Add(temp);
+            }
+            else
+            {
+                foreach (CartItem item in cartItems)
+                {
+                    if (item.GetProduct().name == temp.GetProduct().name)
+                    {
+                        (item.Controls[2] as NumericUpDown).Value++;
+                    }
+                    else
+                    {
+                        cartItems.Add(temp);
+                        leftPanel.Controls.Add(temp);
+                    }
+                }
+            }
         }
     }
 }
