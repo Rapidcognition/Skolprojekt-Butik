2609a718480b45b26eb08f5b45cdf66839ce82f8
Samuel Drysén <samuel.drysen@live.se>
2018-10-20 17:38:38 +0200

    Cart updated

--- Butikv3.5/MainView/CartPanel.cs
+++ Butikv3.5/MainView/CartPanel.cs
@@ -8,57 +8,92 @@ using System.Drawing;
 
 namespace Butikv3._5
 {
-    class CartItem : TableLayoutPanel
-    {
-        public CartItem(Product toAdd)
-        {
-            this.Size = new Size(180, 50);
-            this.Margin = new Padding(1);
-            this.ColumnCount = 3;
-            this.BorderStyle = BorderStyle.FixedSingle;
-            this.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25));
-            this.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50));
-            this.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25));
-            this.MouseEnter += CartItem_MouseEnter;
-            this.MouseLeave += CartItem_MouseLeave;
-
-            PictureBox picture = new PictureBox()
-            {
-                Size = new Size(60, 40),
-                Anchor = AnchorStyles.Left,
-                ImageLocation = toAdd.pictureBox.ImageLocation,
-                SizeMode = PictureBoxSizeMode.Zoom,
-                Margin = new Padding(0),
-            };
-            this.Controls.Add(picture);
-
-            Label name = new Label
-            {
-                Text = toAdd.name,
-                Dock = DockStyle.Top,
-                TextAlign = ContentAlignment.MiddleCenter,
-            };
-            this.Controls.Add(name);
-
-            NumericUpDown itemCount = new NumericUpDown
-            {
-                Dock = DockStyle.Top,
-            };
-            this.Controls.Add(itemCount);
-
-        }
-        private void CartItem_MouseEnter(object sender, EventArgs e)
-        {
-            this.BorderStyle = BorderStyle.Fixed3D;
-        }
-        private void CartItem_MouseLeave(object sender, EventArgs e)
-        {
-            this.BorderStyle = BorderStyle.FixedSingle;
-        }
-    }
-
     class CartPanel : TableLayoutPanel
     {
+        private class CartItem : TableLayoutPanel
+        {
+            private Timer timer;
+            private Label priceLabel;
+            private int price;
+            public NumericUpDown itemCount;
+            
+
+            public CartItem(Product toAdd)
+            {
+                this.Name = toAdd.name;
+                this.Size = new Size(180, 60);
+                this.Margin = new Padding(1);
+                this.BorderStyle = BorderStyle.FixedSingle;
+                this.ColumnCount = 3;
+                this.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20));
+                this.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50));
+                this.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30));
+                this.RowCount = 2;
+                this.Click += CartItem_Click;
+
+                timer = new Timer { Interval = 100 };
+                timer.Tick += Timer_Tick;
+
+                PictureBox picture = new PictureBox()
+                {
+                    Size = new Size(60, 40),
+                    Anchor = AnchorStyles.Left,
+                    ImageLocation = toAdd.pictureBox.ImageLocation,
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                    Margin = new Padding(0),
+                };
+                picture.Click += CartItem_Click;
+                this.Controls.Add(picture);
+                this.SetRowSpan(picture, 2);
+
+                Label name = new Label
+                {
+                    Text = toAdd.name,
+                    Dock = DockStyle.Fill,
+                    TextAlign = ContentAlignment.MiddleCenter,
+                };
+                name.Click += CartItem_Click;
+                this.Controls.Add(name);
+                this.SetRowSpan(name, 2);
+
+                itemCount = new NumericUpDown
+                {
+                    Dock = DockStyle.Fill,
+                    Value = 1,
+                };
+                itemCount.ValueChanged += ItemCount_ValueChanged;
+                this.Controls.Add(itemCount);
+
+                price = toAdd.price;
+                priceLabel = new Label
+                {
+                    Text = (price + "kr"),
+                    Dock = DockStyle.Fill,
+                    TextAlign = ContentAlignment.MiddleCenter,
+                };
+                this.Controls.Add(priceLabel);
+            }
+
+            private void ItemCount_ValueChanged(object sender, EventArgs e)
+            {
+                priceLabel.Text = (itemCount.Value * price).ToString() + "kr";
+                if((sender as NumericUpDown).Value == 0)
+                {
+                    this.Dispose();
+                }
+            }
+            private void Timer_Tick(object sender, EventArgs e)
+            {
+                this.BorderStyle = BorderStyle.FixedSingle;
+                timer.Stop();
+            }
+            private void CartItem_Click(object sender, EventArgs e)
+            {
+                this.BorderStyle = BorderStyle.Fixed3D;
+                timer.Start();
+            }
+        }
+       
         private TableLayoutPanel leftPanel;
         private TableLayoutPanel middlePanel;
         private TableLayoutPanel rightPanel;
@@ -73,13 +108,6 @@ namespace Butikv3._5
             this.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 200));
             this.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
             this.Margin = new Padding(0);
-
-            Panel panel = new Panel
-            {
-                Bounds = this.Bounds,
-                BorderStyle = BorderStyle.FixedSingle,
-            };
-            
             
             leftPanel = new TableLayoutPanel
             {
@@ -101,17 +129,20 @@ namespace Butikv3._5
 
         private void TempAddButton_Click(object sender, EventArgs e)
         {
-            Product toAdd = new Product(0000,"rice","food","Good ol rice", "Placeholder0.png");
-            toAdd.name = "Rice";
-            toAdd.pictureBox.ImageLocation = "Placeholder0.png";
+            Product toAdd = new Product(10, "Rice", "food", "Good ol rice", "Placeholder0.png");
             AddToCart(toAdd);
         }
 
         public void AddToCart(Product toCart)
         {
-            CartItem newCartItem = new CartItem(toCart);
-            leftPanel.Controls.Add(newCartItem);
-            
+            if(leftPanel.Controls.ContainsKey(toCart.name))
+            {
+                (leftPanel.Controls[toCart.name] as CartItem).itemCount.Value++;
+            }
+            else
+            {
+                leftPanel.Controls.Add(new CartItem(toCart));
+            }
         }
     }
 }
