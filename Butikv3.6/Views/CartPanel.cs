d149692487bd75119773983dad83f38fc2d15f88
Samuel Drysén <samuel.drysen@live.se>
2018-11-01 14:26:02 +0100

    ViewPanel inheritance implementation

--- Butikv3.6/Views/CartPanel.cs
+++ Butikv3.6/Views/CartPanel.cs
@@ -9,7 +9,7 @@ using System.Drawing;
 
 namespace Butikv3._6
 {
-    class CartPanel : TableLayoutPanel
+    class CartPanel : ViewPanel
     {
         private double Sum = 0;
         private Label sumBeforDis;
@@ -20,21 +20,10 @@ namespace Butikv3._6
         private const string TempSaveFile = "saveFile.csv";
 
         private FlowLayoutPanel itemPanel;
-        private TableLayoutPanel selectedProductPanel;
         private List<Product> cartItems = new List<Product>();
 
         public CartPanel()
         {
-            this.ColumnCount = 3;
-            this.RowCount = 2;
-            this.Dock = DockStyle.Fill;
-            this.Margin = new Padding(0);
-            this.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 135));
-            this.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100));
-            this.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 200));
-            this.RowStyles.Add(new RowStyle(SizeType.Percent, 92));
-            this.RowStyles.Add(new RowStyle(SizeType.Percent, 8));
-
             #region left menu
             TableLayoutPanel leftMenuPanel = new TableLayoutPanel
             {
@@ -100,7 +89,7 @@ namespace Butikv3._6
                 TextAlign= HorizontalAlignment.Center,
                 BackColor = Color.White,
 
-        };
+            };
             leftMenuPanel.Controls.Add(DiscountCodeBox);
             DiscountCodeBox.GotFocus += ClearText;
             DiscountCodeBox.KeyPress += ChackCode;
@@ -168,48 +157,6 @@ namespace Butikv3._6
                 
             };
             sumOfProductsPanel.Controls.Add(sumLabel);
-            #endregion
-
-            #region Description panel
-            TableLayoutPanel descriptionPanel = new TableLayoutPanel
-            {
-                Name = "descriptionPanel",
-                Dock = DockStyle.Fill,
-                RowCount = 3,
-            };
-            descriptionPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 50));
-            descriptionPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 10));
-            descriptionPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 40));
-            this.SetRowSpan(leftMenuPanel, 2);
-            this.Controls.Add(descriptionPanel, 2, 0);
-
-            PictureBox descriptionPicture = new PictureBox
-            {
-                Name = "descriptionPicture",
-                Dock = DockStyle.Fill,
-                BorderStyle = BorderStyle.Fixed3D,
-                SizeMode = PictureBoxSizeMode.StretchImage,
-            };
-            descriptionPanel.Controls.Add(descriptionPicture);
-
-            Label descriptionProductName = new Label
-            {
-                Name = "descriptionProductName",
-                Text = "No product selected",
-                Dock = DockStyle.Fill,
-                Font = new Font("Calibri", 12, FontStyle.Bold),
-                TextAlign = ContentAlignment.TopLeft,
-            };
-            descriptionPanel.Controls.Add(descriptionProductName);
-
-            Label descriptionProductSummary = new Label
-            {
-                Name = "descriptionProductSummary",
-                Text = " ---",
-                Dock = DockStyle.Fill,
-                TextAlign = ContentAlignment.TopLeft,
-            };
-            descriptionPanel.Controls.Add(descriptionProductSummary);
             #endregion
         }
 
@@ -454,37 +401,6 @@ namespace Butikv3._6
                 UpdateDescriptionPanel(descriptionPanelRef, productRef);
                 UpdateSelectedProduct(productPanelRef);
             }
-        }
-
-        /// <summary>
-        /// Updates selected Product button collapse.
-        /// </summary>
-        private void UpdateSelectedProduct(TableLayoutPanel clickedProductPanelRef)
-        {
-            if(selectedProductPanel == null)
-            {
-                selectedProductPanel = clickedProductPanelRef;
-                selectedProductPanel.BorderStyle = BorderStyle.Fixed3D;
-            }
-
-            if (selectedProductPanel != clickedProductPanelRef)
-            {
-                selectedProductPanel.BorderStyle = BorderStyle.None;
-                selectedProductPanel = clickedProductPanelRef;
-                selectedProductPanel.BorderStyle = BorderStyle.Fixed3D;
-            }
-        }
-
-        /// <summary>
-        /// Updates description panel when a product is selected.
-        /// </summary>
-        private void UpdateDescriptionPanel(TableLayoutPanel descriptionPanelRef, Product productRef)
-        {
-            (descriptionPanelRef.Controls["descriptionPicture"] as PictureBox).ImageLocation = productRef.imageLocation;
-
-            (descriptionPanelRef.Controls["descriptionProductName"] as Label).Text = productRef.name;
-
-            (descriptionPanelRef.Controls["descriptionProductSummary"]).Text = productRef.summary;
         }
 
         /// <summary>
