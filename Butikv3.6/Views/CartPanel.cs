446b9ad29c730d10344c79e957e54661a4580efe
Samuel Drysén <samuel.drysen@live.se>
2018-11-05 14:30:12 +0100

    Added multi-savefile functionality

--- Butikv3.6/Views/CartPanel.cs
+++ Butikv3.6/Views/CartPanel.cs
@@ -17,7 +17,6 @@ namespace Butikv3._6
         private Label sumLabel;
         private bool codeActive = false;
         private const string SaveFolder = "saveFolder";
-        private const string TempSaveFile = "saveFile.csv";
 
         private FlowLayoutPanel itemPanel;
         private List<Product> cartItems = new List<Product>();
@@ -173,53 +172,6 @@ namespace Butikv3._6
             #endregion
         }
 
-        private void ClearText(object sender, EventArgs e)
-        {
-            TextBox textB = (TextBox)sender;
-            if (textB.BackColor != Color.LightGreen)
-            {
-                textB.Clear();
-                textB.BackColor = Color.White;
-                //textB.Hide();
-            }
-            else
-                textB.AcceptsReturn = false;
-
-            
-        }
-
-        private void ChackCode(object sender, EventArgs e)
-        {
-            TextBox txtbcode = (TextBox)sender;
-            List<string> DisCodList = File.ReadAllLines(@"RabatCoder.csv").ToList();
-            foreach (string item in DisCodList)
-            {
-                if (txtbcode.Text == item)
-                {
-                    DisCodList.Remove(item);
-                    txtbcode.BackColor = Color.LightGreen;
-                    sumAfterDis.Text = "Your amout After discount:\n" + GetSumOfProductsAfterDis() + " kr";
-                    sumLabel.Text = "Sum: " + GetSumOfProductsAfterDis() + " kr";
-                    codeActive = true;
-                }
-                break;
-            }
-            //File.Delete(@"RabatCoder.csv");
-            //File.Create(@"RabatCoder.csv");
-            File.WriteAllText(@"RabatCoder.csv", string.Empty);
-            File.WriteAllLines(@"RabatCoder.csv", DisCodList);
-
-            if(codeActive)
-            {
-                txtbcode.Enabled = false;
-            }
-        }
-
-        private void CheckoutButton_Click(object sender, EventArgs e)
-        {
-            
-        }
-
         public void AddToCart(Product product)
         {
             if (itemPanel.Controls.ContainsKey(product.name))
@@ -302,6 +254,53 @@ namespace Butikv3._6
             UpdateSummaryPanel();
         }
 
+        private void ClearText(object sender, EventArgs e)
+        {
+            TextBox textB = (TextBox)sender;
+            if (textB.BackColor != Color.LightGreen)
+            {
+                textB.Clear();
+                textB.BackColor = Color.White;
+                //textB.Hide();
+            }
+            else
+                textB.AcceptsReturn = false;
+
+            
+        }
+
+        private void ChackCode(object sender, EventArgs e)
+        {
+            TextBox txtbcode = (TextBox)sender;
+            List<string> DisCodList = File.ReadAllLines(@"RabatCoder.csv").ToList();
+            foreach (string item in DisCodList)
+            {
+                if (txtbcode.Text == item)
+                {
+                    DisCodList.Remove(item);
+                    txtbcode.BackColor = Color.LightGreen;
+                    sumAfterDis.Text = "Your amout After discount:\n" + GetSumOfProductsAfterDis() + " kr";
+                    sumLabel.Text = "Sum: " + GetSumOfProductsAfterDis() + " kr";
+                    codeActive = true;
+                }
+                break;
+            }
+            //File.Delete(@"RabatCoder.csv");
+            //File.Create(@"RabatCoder.csv");
+            File.WriteAllText(@"RabatCoder.csv", string.Empty);
+            File.WriteAllLines(@"RabatCoder.csv", DisCodList);
+
+            if(codeActive)
+            {
+                txtbcode.Enabled = false;
+            }
+        }
+
+        private void CheckoutButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
         private void ClearCart()
         {
             itemPanel.Controls.Clear();
@@ -325,36 +324,66 @@ namespace Butikv3._6
                 Directory.CreateDirectory(SaveFolder);
             }
 
-            if(cartItems.Count != 0)
+            if(cartItems.Count == 0)
             {
-                string[] lines = new string[cartItems.Count];
+                MessageBox.Show("Cannot save an empty cart.", "༼つಠ益ಠ༽つ", MessageBoxButtons.OK);
+            }
+            else
+            {
+                SaveFileDialog fileDialog = new SaveFileDialog();
+                fileDialog.Filter = "Csv file|*.csv";
+                fileDialog.Title = "Save shopping cart";
+                fileDialog.InitialDirectory = SaveFolder;
 
-                for(int i = 0; i < cartItems.Count; i++)
+                DialogResult result = fileDialog.ShowDialog();
+                string saveFilePath = fileDialog.FileName;
+
+                if (result == DialogResult.Cancel)
                 {
-                    lines[i] = cartItems[i].ToCSV();
+                    MessageBox.Show("Shopping cart was not saved.", "☉ ‿ ⚆", MessageBoxButtons.OK);
                 }
+                else if (result == DialogResult.OK)
+                {
+                    string[] lines = new string[cartItems.Count];
 
-                File.WriteAllLines(SaveFolder + "/" + TempSaveFile, lines);
+                    for (int i = 0; i < cartItems.Count; i++)
+                    {
+                        lines[i] = cartItems[i].ToCSV();
+                    }
+
+                    File.WriteAllLines(saveFilePath, lines);
+                }
             }
         }
 
         private void LoadCartButton_Click(object sender, EventArgs e)
         {
-            if (!Directory.Exists(SaveFolder)) 
+            if (!Directory.Exists(SaveFolder))
             {
                 Directory.CreateDirectory(SaveFolder);
             }
 
-            ClearCart();
-            if (File.Exists(SaveFolder + "/" + TempSaveFile)) 
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Csv file|*.csv";
+            fileDialog.Title = "Read from save file";
+            fileDialog.InitialDirectory = SaveFolder;
+
+            DialogResult result = fileDialog.ShowDialog();
+            if(result == DialogResult.Cancel)
             {
-                var tmp = File.ReadAllLines(SaveFolder + "/" + TempSaveFile).
+                MessageBox.Show("No save file selected.", @"¯\_(ツ)_/¯", MessageBoxButtons.OK);
+            }
+            else if(result == DialogResult.OK)
+            {
+                ClearCart();
+                string saveFilePath = fileDialog.FileName;
+                var saveFileContents = File.ReadAllLines(saveFilePath).
                     Select(x => Product.FromCSV(x)).
                     OrderBy(x => x.name).OrderBy(x => x.type).ToList();
 
-                foreach (Product item in tmp)
+                foreach (Product product in saveFileContents)
                 {
-                    AddToCart(item);
+                    AddToCart(product);
                 }
             }
         }
