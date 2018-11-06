481ad2ff51136d893815480d5a55b49a29564f01
Rapidcognition <37350380+Rapidcognition@users.noreply.github.com>
2018-11-06 18:05:45 +0100

    Made it display visually

--- Butikv3.6/Views/HomePanel.cs
+++ Butikv3.6/Views/HomePanel.cs
@@ -15,8 +15,9 @@ namespace Butikv3._6
         public HomePanel(CartPanel reference)
         {
             cartPanelRef = reference;
-            PopulateHomePanelList();
             #region specs
+            this.Dock = DockStyle.Fill;
+            this.Margin = new Padding(0);
             this.ColumnCount = 3;
             this.RowCount = 2;
             this.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33));
@@ -25,6 +26,9 @@ namespace Butikv3._6
             this.RowStyles.Add(new RowStyle(SizeType.Percent, 10));
             this.RowStyles.Add(new RowStyle(SizeType.Percent, 90));
             #endregion
+
+            PopulateHomePanelList();
+            PopulateHomePanel();
 
         }
         public void PopulateHomePanelList()
@@ -67,6 +71,64 @@ namespace Butikv3._6
                             p.stage2 = true;
                     }
                 }
+            }
+        }
+
+        public void PopulateHomePanel()
+        {
+            int counter = 0;
+            foreach (List<Product> listItem in mainProductList)
+            {
+                Label title = new Label { Text = listItem[counter].type};
+                this.Controls.Add(title, counter, 0);
+                FlowLayoutPanel panel = new FlowLayoutPanel
+                {
+                    FlowDirection = FlowDirection.LeftToRight,
+                    Dock = DockStyle.Fill,
+                };
+                this.Controls.Add(panel);
+                foreach (Product item in listItem)
+                {
+                    TableLayoutPanel productPanel = new TableLayoutPanel
+                    {
+                        ColumnCount = 3,
+                        Anchor = AnchorStyles.Top,
+                        Height = 60,
+                        Width = 300,
+                        Margin = new Padding(0),
+                    };
+                    productPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33));
+                    productPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33));
+                    productPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33));
+                    productPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100));
+                    panel.Controls.Add(productPanel);
+
+                    PictureBox itemPictureBox = new PictureBox
+                    {
+                        BorderStyle = BorderStyle.Fixed3D,
+                        SizeMode = PictureBoxSizeMode.StretchImage,
+                        Dock = DockStyle.Top,
+                        Image = Image.FromFile(item.imageLocation),
+                    };
+                    productPanel.Controls.Add(itemPictureBox);
+                    Label itemNameLabel = new Label
+                    {
+                        Text = item.name,
+                        TextAlign = ContentAlignment.MiddleLeft,
+                        Anchor = AnchorStyles.Left,
+                        Font = new Font("Calibri", 10, FontStyle.Bold),
+                    };
+                    productPanel.Controls.Add(itemNameLabel);
+                    Label itemPriceLabel = new Label
+                    {
+                        Text = item.price + "kr",
+                        TextAlign = ContentAlignment.MiddleLeft,
+                        Anchor = AnchorStyles.Left,
+                        Font = new Font("Calibri", 9, FontStyle.Bold),
+                    };
+                    productPanel.Controls.Add(itemPriceLabel);
+                }
+                counter++;
             }
         }
     }
